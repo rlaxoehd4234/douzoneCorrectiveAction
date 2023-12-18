0543a088c1b66146b7803aaafa5510f7b9801c5b
김태동 <kimtaedong@douzone.com>
2023-12-18 09:48:12 +0900

    [REFACTOR] Update Dews File, Change Version on dews file , Union file

---
+++ douzone-comet-service-eh-abc/douzone-comet-service-eh-abc/src/main/java/com/douzone/comet/service/eh/abc/Abc00400Service.java
@@ -0,0 +1,87 @@
+package com.douzone.comet.service.eh.abc;
+ 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.douzone.comet.components.DzCometService;
+import com.douzone.comet.service.eh.abc.dao.Abc00400DAO;
+import com.douzone.comet.service.eh.abc.models.Abc00400DtlModel;
+import com.douzone.comet.service.eh.abc.models.Abc00400MstModel;
+import com.douzone.gpd.components.exception.DzApplicationRuntimeException;
+import com.douzone.gpd.restful.annotation.DzApi;
+import com.douzone.gpd.restful.annotation.DzApiService;
+import com.douzone.gpd.restful.annotation.DzParam;
+import com.douzone.gpd.restful.enums.CometModule;
+import com.douzone.gpd.restful.enums.DzParamType;
+import com.douzone.gpd.restful.enums.DzRequestMethod;
+
+/** 
+  * @description : 시정조치관리 서비스
+  * @Since   : 2023/12/10
+  * @Author  : 김태동
+  * @History : 2023/12/15 코드최적화, 검토자 : 김태동
+  */
+@DzApiService(value="Abc00400Service", module=CometModule.EH, desc="시정조치요구관리")
+public class Abc00400Service extends DzCometService {
+
+	@Autowired
+	Abc00400DAO abc00400DAO; 
+
+	@DzApi(url="/abc00400Mst_list", desc="시정조치요구관리마스터-조회", httpMethod=DzRequestMethod.GET)
+	public List<Abc00400MstModel> abc00400Mst_list(
+		@DzParam(key="companyCode", desc="회사코드", paramType = DzParamType.QueryString) String companyCode,
+		@DzParam(key="CRCT_TRMT_NM", desc="시정조치제목", paramType = DzParamType.QueryString) String CRCT_TRMT_NM,
+		@DzParam(key="CRCT_TRMT_REQN_DT_START", desc="발행일자(시작일)", paramType = DzParamType.QueryString) String CRCT_TRMT_REQN_DT_START,
+		@DzParam(key="CRCT_TRMT_REQN_DT_END", desc="발행일자(종료일)", paramType = DzParamType.QueryString) String CRCT_TRMT_REQN_DT_END,
+		@DzParam(key="REVIEW_RST_FG", desc="상태", paramType = DzParamType.QueryString) String REVIEW_RST_FG
+	) throws Exception {
+		List<Abc00400MstModel> abc00400MstModelList =  new ArrayList<Abc00400MstModel>();
+		try {
+	 		HashMap<String, Object> param = new HashMap<String, Object>();
+			
+			param.put("P_COMPANY_CD", companyCode);
+			param.put("P_CRCT_TRMT_NM", CRCT_TRMT_NM);
+			param.put("P_CRCT_TRMT_REQN_DT_START", CRCT_TRMT_REQN_DT_START);
+			param.put("P_CRCT_TRMT_REQN_DT_END", CRCT_TRMT_REQN_DT_END);
+			param.put("P_REVIEW_RST_FG", REVIEW_RST_FG);
+//	         400 마스터 그리드 검색 조회
+	 		abc00400MstModelList = abc00400DAO.selectAbc00400MstModelList(param);
+//          셋 디테일을 위한 UUID 속성 추가
+	 		for (Abc00400MstModel item : abc00400MstModelList) {
+				item.set_uid(UUID.randomUUID().toString());
+			}
+	 	} catch(Exception e) {
+	        throw new DzApplicationRuntimeException(e);
+	    }
+	    
+	    return abc00400MstModelList;
+ 	}
+ 	
+ 	@DzApi(url="/abc00400Dtl_list", desc="시정조치요구관리디테일-조회", httpMethod=DzRequestMethod.GET)
+	public List<Abc00400DtlModel> abc00400Dtl_list(
+		@DzParam(key="companyCode", desc="회사코드", paramType = DzParamType.QueryString) String companyCode,
+		@DzParam(key="CRCT_TRMT_NO", desc="시정조치제목", paramType = DzParamType.QueryString) String CRCT_TRMT_NO
+	) throws Exception {
+		List<Abc00400DtlModel> abc00400DtlModelList =  new ArrayList<Abc00400DtlModel>();
+		try {
+	 		HashMap<String, Object> param = new HashMap<String, Object>();
+			
+			param.put("P_COMPANY_CD", companyCode);
+			param.put("P_CRCT_TRMT_NO", CRCT_TRMT_NO);
+//	         400 디테일 그리드 검색 조회
+	 		abc00400DtlModelList = abc00400DAO.selectAbc00400DtlModelList(param);
+	 		
+	 		for (Abc00400DtlModel item : abc00400DtlModelList) {
+				item.set_uid(UUID.randomUUID().toString());
+			}
+	 	} catch(Exception e) {
+	        throw new DzApplicationRuntimeException(e);
+	    }
+	    
+	    return abc00400DtlModelList;
+ 	}
+}
